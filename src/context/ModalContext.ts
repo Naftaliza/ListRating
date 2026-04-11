ea17f1a94f22cd315c5c2386d60d56d8052fd565
Naftaliza <naftalizaro@gmail.com>
2026-04-11 10:13:27 +0300

    Updates

--- src/context/ModalContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext } from 'react'
-import type { Pokemon } from '../types/pokemon'
-
-export const ModalContext = createContext<(pokemon: Pokemon) => void>(() => {})
-
-export function useModal() {
-  return useContext(ModalContext)
-}
